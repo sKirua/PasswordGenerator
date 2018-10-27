b78c2ca5179de95c5aa8b585db8edc31905ccdc2
sKirua <43117514+sKirua@users.noreply.github.com>
2018-10-27 16:58:00 +0200

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,6 +1,6 @@
 
 import random
-char = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+char = "a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9" 
 char_sep = char.split(" ")
 def gen_password(pass_len):
     password = "" 
@@ -14,7 +14,16 @@ def gen_password(pass_len):
     
 continue_prog = True 
 while continue_prog:
-    print("Vespa Password Generator ")
+    print("""
+ __      __                     _____                                    _    _____                           _             
+ \ \    / /                    |  __ \                                  | |  / ____|                         | |            
+  \ \  / /__  ___ _ __   __ _  | |__) |_ _ ___ _____      _____  _ __ __| | | |  __  ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
+   \ \/ / _ \/ __| '_ \ / _` | |  ___/ _` / __/ __\ \ /\ / / _ \| '__/ _` | | | |_ |/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
+    \  /  __/\__ \ |_) | (_| | | |  | (_| \__ \__  \ V  V / (_) | | | (_| | | |__| |  __/ | | |  __/ | | (_| | || (_) | |   
+     \/ \___||___/ .__/ \__,_| |_|   \__,_|___/___/ \_/\_/ \___/|_|  \__,_|  \_____|\___|_| |_|\___|_|  \__,_|\__\___/|_|   
+                 | |                                                                                                        
+                 |_|                                                                                                        
+              """)
     print("By Kirua for Vespa Corporation")
     print("Choisi la longeur du mdp boufon:")
     user_len_password = int(input())
@@ -22,6 +31,6 @@ while continue_prog:
     print("Voici ton mdp macaque :", final_password)
     print("Tu as fini ? o/n")
     choice = input()
-    #pour choisir de redemarer ou pas
-    if choice == "o":
+if choice == "o":
         continue_prog = False
+        
