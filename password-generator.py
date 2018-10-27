02adabe803654d046da733d1d9ad2dd49d655c0a
sKirua <43117514+sKirua@users.noreply.github.com>
2018-10-27 18:23:50 +0200

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,6 +1,6 @@
 
 import random
-char = "a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9" 
+char = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9" 
 char_sep = char.split(" ")
 def gen_password(pass_len):
     password = "" 
@@ -33,4 +33,3 @@ while continue_prog:
     choice = input()
 if choice == "o":
         continue_prog = False
-        
